918c1caceb1fd6d4518f74334158f22b1556b7cc
Czareg <czarecoo@gmail.com>
2024-02-21 10:37:56 +0100

    Fixed free time config

--- src/main/java/com/czareg/school/SchoolApplication.java
+++ src/main/java/com/czareg/school/SchoolApplication.java
@@ -1,9 +1,12 @@
 package com.czareg.school;
 
+import com.czareg.school.config.FreeTimeSlotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FreeTimeSlotConfig.class)
 public class SchoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/czareg/school/config/FreeTimeSlotConfig.java
+++ src/main/java/com/czareg/school/config/FreeTimeSlotConfig.java
@@ -3,22 +3,26 @@ package com.czareg.school.config;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-@Configuration
 @ConfigurationProperties(prefix = "free.time.slot")
 @Getter
+@RequiredArgsConstructor
 public class FreeTimeSlotConfig {
 
-    @Min(0) @Max(23)
-    int startHour;
-    @Min(0) @Max(59)
-    int startMinute;
-    @Min(0) @Max(23)
-    int endHour;
-    @Min(0) @Max(59)
-    int endMinute;
+    @Min(0)
+    @Max(23)
+    private final int startHour;
+    @Min(0)
+    @Max(59)
+    private final int startMinute;
+    @Min(0)
+    @Max(23)
+    private final int endHour;
+    @Min(0)
+    @Max(59)
+    private final int endMinute;
 }
